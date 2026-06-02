0350d88030736e6fdb72d8a6047401f1bc77c8c9
Miquel Vera <miveradev@gmail.com>
2026-06-02 20:35:33 +0200

    fix: kysley-adapter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["better-auth"],
+  serverExternalPackages: ["better-auth", "@better-auth/kysely-adapter", "kysely"],
 };
 
 export default nextConfig;
